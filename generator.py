c3de159141fd5384b219905b2e387bd837a63a66
asher_gingerich@taylor.edu <agingeri@cse21704.cs.local>
2019-04-05 20:39:07 -0400

    Base level grammar generation, needs lots of bug fixing

--- generator.py
+++ generator.py
@@ -1,5 +1,31 @@
 import random
 
+grammar = {
+    "sentence":[("init", "VP")],
+    "NP":[("det", "N"), ("det", "adj", "N")],
+    "VP":[("V"), ("V", "adV"), ("V", "NP"), ("adV", "V"), ("adV", "V", "NP")],
+    "init":["Our mission is to", "Our company's goal is to"],
+    #"N":["farm", "child", "plan", "client's need"],
+    "N":["farms", "children", "plans", "clients' needs"],
+    #"det":["the", "our", "a", "their", "your"],
+    "det":["the", "our", "all", "those", "their"],
+    "V": ["help", "inspire", "craft", "drive", "aggregate", "compile"],
+    "adj": ["poor", "sick", "disordered", "clean", "friendly", "nice"],
+    "adV": ["creatively", "effeciently", "quickly", "correctly", "cleanly"]
+}
+
+def generation(token):
+    if token in grammar:
+        return generation(grammar[token][(random.randrange(0, len(grammar[token])))])
+    if isinstance(token, list):
+        return generation(token[random.randrange(0,len(token))])
+    if isinstance(token, tuple):
+        finishedPhrase = ""
+        for phrase in token:
+            finishedPhrase += generation(phrase) + " "
+        return finishedPhrase
+    return token
+
 def randNoun():
     r = random.randrange(0, 3) 
     if r == 0:
@@ -42,4 +68,14 @@ def template():
     else:
         print("Fail")
 
-template()
+def main():
+    while True:
+        chosenGenerator = input("Which generator do you want? (T or G): ")
+        if chosenGenerator == "T":
+            template()
+            break
+        elif chosenGenerator == "G":
+            print(generation("sentence"))
+            break
+    
+main()
