e17313c811abbbe737071b4ae75fb079a1337ce1
EMcMasters <31521362+EMcMasters@users.noreply.github.com>
2019-05-03 23:15:54 -0400

    Update mazeRunner.ino
    currently does not stop at an intersection

--- mazeRunner.ino
+++ mazeRunner.ino
@@ -5,31 +5,34 @@
 Servo myservo; // create servo object to control servo
 int Echo = A4;  
 int Trig = A5; 
+
 int in1 = 6;
 int in2 = 7;
 int in3 = 8;
 int in4 = 9;
+
 int ENA = 5;
 int ENB = 11;
-int ABS = 150;
-int angle = 0, distLeft = 0, distRight = 0;
+
+int ABS = 120;
+
+int angle = 0, distLeft = 0, distRight = 0, distCenter = 0;
 int turnDelay = 650;
 int bias = 0;
 
-/*//Our car
+//Our car
 int SENSE_LEFT=2;
 int SENSE_CENTER=4;
 int SENSE_RIGHT=10;
-*/
-
-//borrowed car
-int SENSE_LEFT=10;
-int SENSE_CENTER=4;
-int SENSE_RIGHT=2;
 
 
 bool LIGHT=false;
 bool DARK=true;
+
+unsigned long time;
+unsigned long delayStart;
+long delayTime = 3500;
+bool turning = false;
 
 void forward()
 {
@@ -39,7 +42,16 @@ void forward()
   digitalWrite(in2,LOW);
   digitalWrite(in3,LOW);
   digitalWrite(in4,HIGH);
- Serial.println("go forward!");
+ //Serial.println("go forward!");
+}
+
+void backward(){
+  analogWrite(ENA,ABS);
+  analogWrite(ENB,ABS);
+  digitalWrite(in1,LOW);
+  digitalWrite(in2,HIGH);
+  digitalWrite(in3,HIGH);
+  digitalWrite(in4,LOW);
 }
 
 void back()
@@ -98,7 +110,7 @@ void halt()
 {
   digitalWrite(ENA,LOW);
   digitalWrite(ENB,LOW);
-  Serial.println("Stop!");
+  //Serial.println("Stop!");
 } 
  /*Ultrasonic distance measurement Sub function*/
 int Distance_test()   
@@ -137,7 +149,9 @@ void loop() {
   //TODO: Make decision on direction at intersections (both color sensors tripped)
   //TODO: Include forward sonar in decision
   //TODO: Implement bias
-  if(Distance_test() < 15){
+  time = millis();
+  halt();
+  /*if(Distance_test() < 15){
     halt();
     angle = 180;
     myservo.write(180);
@@ -164,22 +178,83 @@ void loop() {
     angle = 90;
     myservo.write(angle);
     delay(1000);
-  }
+  }*/
   //delay(100);
   int senseLeft = digitalRead(SENSE_LEFT);
   int senseRight = digitalRead(SENSE_RIGHT);
-  if(senseLeft==DARK) {
-    Serial.println("GO LEFT"); 
-    right();
-    //_mleft();
-    delay(15);
-  }else if(senseRight==DARK) {
-    Serial.println("GO RIGHT");
+  int senseCenter = digitalRead(SENSE_CENTER);
+  if (turning){
+      Serial.println(delayStart);
+    if ((time - delayStart) >= delayTime){
+        turning = false;
+        Serial.print("start of loop: ");
+        Serial.println(turning);
+      }
+    }
+  if (senseLeft==DARK and senseRight==DARK and senseCenter==DARK and not turning){
+    Serial.println("look");
+    halt();
+    angle = 180;
+    myservo.write(180);
+    delay(500);
+    distLeft = Distance_test();
+    Serial.println(distLeft);
+    delay(100);
+    angle = 0;
+    myservo.write(0);
+    delay(500);
+    distRight = Distance_test();
+    Serial.println(distRight);
+    delay(100);
+    angle = 90;
+    myservo.write(90);
+    delay(500);
+    distCenter = Distance_test();
+    Serial.println(distRight);
+    delay(100);
+    if(distRight > 15 and distRight > distLeft and distRight > distCenter) {
+      turning = true;
+      delayStart = time;
+      right();
+    }else if(distLeft > 15 and distLeft > distCenter) {
+      turning = true;
+      delayStart = time;
+      left();
+    }else if(distCenter > 15){
+      forward();
+    }else if(distRight > 15){
+      turning = true;
+      delayStart = time;
+      right();
+    }else if(distLeft > 15){
+      turning = true;
+      delayStart = time;
+      left();
+    }else {
+      back();
+      delay(10);
+    }
+    delay(1000);
+  }else if(senseLeft==DARK) {
+    if(not turning){
+      turning = true;
+      delayStart = time;
+      Serial.print("at turn: ");
+      Serial.println(turning);
+      }
     left();
-    //_mright();
-    delay(15);
-  }else{
+  }else if(senseRight==DARK) {
+    if(not turning){
+      turning = true;
+      delayStart = time;
+      Serial.print("at turn: ");
+      Serial.println(turning);
+    }
+    right();
+  }else if(senseCenter==DARK){
     forward();
+  }else{
+    backward();
   }
+  delay(30);
 }
-
